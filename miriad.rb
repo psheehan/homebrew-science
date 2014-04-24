d7b4dfc8388a9579f9e9adf33926f6f907e151c5
Patrick Sheehan <psheehan287@gmail.com>
2014-04-24 11:14:22 -0700

    Updated to the newest version of MIRIAD

--- miriad.rb
+++ miriad.rb
@@ -2,12 +2,12 @@ require 'formula'
 
 class Miriad < Formula
   homepage ''
-  url 'https://www.cfa.harvard.edu/~pwilliam/miriad-macport/miriad-4.3.7.20130915.tar.gz'
-  sha1 'ea882fd993e808af98932e75f4990b97ea9f0669'
+  url 'https://www.cfa.harvard.edu/~pwilliam/miriad-macport/miriad-4.3.7.20131227.tar.gz'
+  sha1 '39c614e3ad9e132231ccd215feea6abb054a8568'
   keg_only "This is just the way that MIRIAD works."
 
-  depends_on :x11
-  depends_on :fortran
+  #depends_on :x11
+  #depends_on :fortran
 
   def install
     ENV.deparallelize
