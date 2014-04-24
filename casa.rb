3104e630fb2cb8ba99877a6a65efb2a14660b39a
Patrick Sheehan <psheehan287@gmail.com>
2014-04-24 11:35:13 -0700

    Updated to use the latest stable CASA version.

--- casa.rb
+++ casa.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Casa < Formula
   homepage 'http://casa.nrao.edu/'
-  url 'https://svn.cv.nrao.edu/casa/linux_distro/stable/casapy-stable-42.0.26465-001-64b.tar.gz', :using => :curl
-  sha1 '6ddcffce471ad517b035d2d6a9ae5c4dd67ea985'
-  version '4.1.0'
+  url 'https://svn.cv.nrao.edu/casa/linux_distro/stable/casapy-stable-42.0.26465-011-64b.tar.gz', :using => :curl
+  sha1 '76180f90f0ac4ed75b19e260351b3e0bffae2e02'
+  version '4.2.11'
 
   def install
     prefix.install Dir['*']
