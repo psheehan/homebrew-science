8abca8d804d8d468d51a617f9bb7754058479abd
Patrick Sheehan <psheehan287@gmail.com>
2014-06-24 18:32:51 -0700

    Updated formula for radmc3d to radmc3d version 0.38.

--- radmc3d.rb
+++ radmc3d.rb
@@ -2,16 +2,16 @@ require 'formula'
 
 class Radmc3d < Formula
   homepage 'http://www.ita.uni-heidelberg.de/~dullemond/software/radmc-3d/'
-  url 'http://kvothe.as.arizona.edu/~psheehan/repository/radmc3d-0.35.zip'
-  sha1 'bf6959ee6043c3b4dbaf30d7ef816972aed4c83c'
+  url 'http://kvothe.as.arizona.edu/~psheehan/public_html/repository/radmc3d-0.38.zip'
+  sha1 '1753b34648f096b360e301920b16fed6348e518c'
 
   depends_on :fortran
 
   def install
     ENV.deparallelize
     ENV.no_optimization
-    system "make", "-C", "version_0.35/src/"
+    system "make", "-C", "version_0.38/src/"
     system "mkdir", "-p", "#{prefix}/bin"
-    system "cp", "version_0.35/src/radmc3d", "#{prefix}/bin/"
+    system "cp", "version_0.38/src/radmc3d", "#{prefix}/bin/"
   end
 end
