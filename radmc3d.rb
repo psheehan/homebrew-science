5108f9280c0836c3436e3a8a887d382b2f086f1a
Patrick Sheehan <psheehan287@gmail.com>
2014-07-15 13:58:44 -0700

    Updated the formula for radmc3d to include a bugfix for the MRW part of the code.

--- radmc3d.rb
+++ radmc3d.rb
@@ -18,6 +18,11 @@ class Radmc3d < Formula
         inreplace 'version_0.38/src/Makefile', 'OPTIM = -O2', 'OPTIM = -O2 -fopenmp'
     end
 
+    # Hopefully fix a bug in the MRW part of the code.
+    inreplace 'version_0.38/src/montecarlo_module.f90', 'mrw_cell_uses_mrw(ray_index) = .true.', '!mrw_cell_uses_mrw(ray_index) = .true.'
+    #inreplace 'version_0.38/src/montecarlo_module.f90', '(r.lt.cellx0(1)).or.(r.gt.cellx1(1))', '(r.lt.cellx0(1)*(1.0-1d-11)).or.(r.gt.cellx1(1)*(1.0+1d-11))'
+    #inreplace 'version_0.38/src/montecarlo_module.f90', "write(stdo,*) '  r = ',r,', range = ',cellx0(1),cellx1(1)", "write(stdo,*) '  r = ',r,', range = ',cellx0(1)*(1.0-1d-11),cellx1(1)*(1.0+1d-11)"
+
     system "make", "-C", "version_0.38/src/"
 
     bin.install "version_0.38/src/radmc3d"
