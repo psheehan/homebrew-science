7233da6eef4c44127e950383f8af2333a20bc41f
Patrick Sheehan <psheehan287@gmail.com>
2014-09-12 16:33:54 -0700

    Added the newest version of CASA.

--- casa.rb
+++ casa.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Casa < Formula
   homepage 'http://casa.nrao.edu/'
-  url 'https://svn.cv.nrao.edu/casa/linux_distro/casapy-42.0.28322-021-1-64b.tar.gz', :using => :curl
+  url 'https://svn.cv.nrao.edu/casa/linux_distro/casapy-42.2.30986-1-64b.tar.gz', :using => :curl
   sha1 '524ff56c0bfcb811bbbef9238f220b08192de17f'
-  version '4.2.21'
+  version '4.2.22'
 
   def install
     prefix.install Dir['*']
