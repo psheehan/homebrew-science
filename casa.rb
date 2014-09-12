6adef4278d452a592d5543b782221096b31cc010
Patrick Sheehan <psheehan287@gmail.com>
2014-09-12 16:51:40 -0700

    Fixed sha1 for the new CASA version.

--- casa.rb
+++ casa.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Casa < Formula
   homepage 'http://casa.nrao.edu/'
   url 'https://svn.cv.nrao.edu/casa/linux_distro/casapy-42.2.30986-1-64b.tar.gz', :using => :curl
-  sha1 '524ff56c0bfcb811bbbef9238f220b08192de17f'
+  sha1 '2bc2ff0132e48a6dd831c55a023c789e110db4ed'
   version '4.2.22'
 
   def install
