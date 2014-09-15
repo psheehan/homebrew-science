99304026fa21c6e4cbd162c45a670ba2ca30e1a4
Patrick Sheehan <psheehan287@gmail.com>
2014-09-15 09:53:29 -0700

    Updated the way CASA version numbering goes.

--- casa.rb
+++ casa.rb
@@ -4,7 +4,7 @@ class Casa < Formula
   homepage 'http://casa.nrao.edu/'
   url 'https://svn.cv.nrao.edu/casa/linux_distro/casapy-42.2.30986-1-64b.tar.gz', :using => :curl
   sha1 '2bc2ff0132e48a6dd831c55a023c789e110db4ed'
-  version '4.2.22'
+  version '4.2.2.30986'
 
   def install
     prefix.install Dir['*']
