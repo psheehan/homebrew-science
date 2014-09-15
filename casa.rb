d58c41edc45b5fc5446f58befd77638e33f6b116
Patrick Sheehan <psheehan287@gmail.com>
2014-09-15 10:16:31 -0700

    Added --with-system-xml option to CASA install

--- casa.rb
+++ casa.rb
@@ -6,7 +6,13 @@ class Casa < Formula
   sha1 '2bc2ff0132e48a6dd831c55a023c789e110db4ed'
   version '4.2.2.30986'
 
+  option "with-system-xml", "Use the system version of xml rather than the one packaged with CASA."
+
   def install
     prefix.install Dir['*']
+
+    if build.with? "system-xml"
+        system "rm", "#{prefix}/lib64/libxml2.so.2"
+        system "ln", "-sf", "/usr/lib64/libxml2.so.2.7.7", "#{prefix}/lib64/libxml2.so.2"
   end
 end
