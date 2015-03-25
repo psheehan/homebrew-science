10497a9597d97c0158e0e27199ee1c3dc52cff3b
Patrick Sheehan <psheehan287@gmail.com>
2015-03-25 10:02:08 -0700

    Updated to RADMC-3D version 0.39

--- radmc3d.rb
+++ radmc3d.rb
@@ -2,28 +2,28 @@ require 'formula'
 
 class Radmc3d < Formula
   homepage 'http://www.ita.uni-heidelberg.de/~dullemond/software/radmc-3d/'
-  url 'http://www.ita.uni-heidelberg.de/~dullemond/software/radmc-3d/radmc-3d_v0.38_17.06.14.zip'
-  sha1 '1753b34648f096b360e301920b16fed6348e518c'
-  version '0.38'
+  url 'http://www.ita.uni-heidelberg.de/~dullemond/software/radmc-3d/radmc-3d_v0.39_17.03.15.zip'
+  sha1 '1dc6477f931a6e2e8bd2fc3f9db562f4b0812035'
+  version '0.39'
 
   depends_on :fortran
 
   option "with-openmp", "Include OpenMP support for parallel processing."
 
-  patch :DATA
-  patch :p0, :DATA
+  #patch :DATA
+  #patch :p0, :DATA
 
   def install
     ENV.deparallelize
     ENV.no_optimization
 
     if build.with? "openmp"
-        inreplace 'version_0.38/src/Makefile', 'OPTIM = -O2', 'OPTIM = -O2 -fopenmp'
+        inreplace 'version_0.39/src/Makefile', 'OPTIM = -O2', 'OPTIM = -O2 -fopenmp'
     end
 
-    system "make", "-C", "version_0.38/src/"
+    system "make", "-C", "version_0.39/src/"
 
-    bin.install "version_0.38/src/radmc3d"
+    bin.install "version_0.39/src/radmc3d"
   end
 end
 
