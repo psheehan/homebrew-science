90916299c87a31d994d955283f2412c68cd73c9a
Patrick Sheehan <psheehan287@gmail.com>
2015-04-13 13:31:50 -0700

    Updated CASA to 4.3.1

--- casa.rb
+++ casa.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Casa < Formula
   homepage 'http://casa.nrao.edu/'
-  url 'https://svn.cv.nrao.edu/casa/linux_distro/casapy-42.2.30986-1-64b.tar.gz', :using => :curl
-  sha1 '2bc2ff0132e48a6dd831c55a023c789e110db4ed'
-  version '4.2.2.30986'
+  url 'https://svn.cv.nrao.edu/casa/linux_distro/release/el6/casa-release-4.3.1-el6.tar.gz', :using => :curl
+  sha1 'a41999fbf649aaf60fd04f85de302a8912693320'
+  version '4.3.1'
 
   option "with-system-xml", "Use the system version of xml rather than the one packaged with CASA."
 
