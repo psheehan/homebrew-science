2ea300a02c2584f976eda5994af83a9c16aff4fd
Patrick Sheehan <psheehan287@gmail.com>
2016-07-27 10:45:36 -0700

    Updated the CASA 4.6.0 install routine

--- casa.rb
+++ casa.rb
@@ -5,6 +5,7 @@ class Casa < Formula
   url 'https://svn.cv.nrao.edu/casa/distro/linux/release/el5/casa-release-4.6.0-el5.tar.gz', :using => :curl
   sha1 'c35154cce48c7074a70956c6a457ac8bb319e7ef'
   version '4.6.0'
+  keg_only 'Because it comes with a lot of preinstalled libraries I dont want to link'
 
   option "with-system-xml", "Use the system version of xml rather than the one packaged with CASA."
 
