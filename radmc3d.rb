1e66e7f901ab2f1d9cdd13c40e3128e86fbd18e5
Patrick Sheehan <psheehan287@gmail.com>
2016-09-23 14:55:19 -0700

    Fixed the radmc3d version 0.40 formula

--- radmc3d.rb
+++ radmc3d.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Radmc3d < Formula
   homepage 'http://www.ita.uni-heidelberg.de/~dullemond/software/radmc-3d/'
   url 'http://www.ita.uni-heidelberg.de/~dullemond/software/radmc-3d/radmc-3d_v0.40_17.09.16.zip'
-  sha128 '7d36839054ad134580bc79bf51172fc0d4688bac'
+  sha256 '24647d73fc2cb5c61c99db4b53ca64ff136e8e707317e708e5f22a70e08d423c'
   version '0.40'
 
   depends_on :fortran
@@ -15,12 +15,12 @@ class Radmc3d < Formula
     ENV.no_optimization
 
     if build.with? "openmp"
-        inreplace 'version_0.39/src/Makefile', 'OPTIM = -O2', 'OPTIM = -O2 -fopenmp'
+        inreplace 'version_0.40/src/Makefile', 'OPTIM = -O2', 'OPTIM = -O2 -fopenmp'
     end
 
-    system "make", "-C", "version_0.39/src/"
+    system "make", "-C", "version_0.40/src/"
 
-    bin.install "version_0.39/src/radmc3d"
+    bin.install "version_0.40/src/radmc3d"
   end
 end
 
