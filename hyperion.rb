91441fcab7a39a2697529296f2dc14047b902dbd
Patrick Sheehan <psheehan287@gmail.com>
2017-09-12 14:52:44 -0500

    Updated Hyperion to version 0.9.9

--- hyperion.rb
+++ hyperion.rb
@@ -2,15 +2,14 @@ require 'formula'
 
 class Hyperion < Formula
   homepage 'www.hyperion-rt.org'
-  url 'https://pypi.python.org/packages/source/H/Hyperion/Hyperion-0.9.4.tar.gz'
-  sha256 '01eee8bdcc73b075a252a6e13a315d1e31b6afab'
+  url 'https://pypi.python.org/packages/4f/6a/9693382f131369526c10d396a2371db1782856bed03ebe8500c89939114e/Hyperion-0.9.9.tar.gz'
+  sha256 'e763a99370b1f961fa2c99b8f5c69cdd618787079af6c6cd63de6a953137e501'
 
-  depends_on 'hdf5' => '--enable-fortran'
-  depends_on 'mpich2'
+  depends_on 'hdf5@1.8'
+  depends_on 'mpich'
 
   def install
     ENV.deparallelize
-    ENV.no_optimization
     system "./configure", "--disable-debug",
                           "--prefix=#{prefix}"
     system "make"
