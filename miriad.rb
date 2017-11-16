c6423bc4d5695cbbf77bb64a708a534374fced11
Patrick Sheehan <psheehan287@gmail.com>
2017-11-16 14:36:03 -0600

    Fixed some bugs with the miriad formula

--- miriad.rb
+++ miriad.rb
@@ -3,15 +3,13 @@ require 'formula'
 class Miriad < Formula
   homepage ''
   url 'https://www.cfa.harvard.edu/~pwilliam/miriad-macport/miriad-4.3.7.20131227.tar.gz'
-  sha256 '39c614e3ad9e132231ccd215feea6abb054a8568'
+  sha256 'f987c616091b889c850623437886c304dfc058781822f8d5fe943d10d4527401'
   keg_only "This is just the way that MIRIAD works."
 
-  #depends_on :x11
-  #depends_on :fortran
+  depends_on 'gcc'
 
   def install
     ENV.deparallelize
-    ENV.no_optimization
 
     system "./configure", "--prefix=#{prefix}", "--with-telescope=carma"
     system "make"
