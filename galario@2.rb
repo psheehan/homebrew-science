a897fa073e1423b43d11aa7d347c55ae493172f9
Patrick Sheehan <psheehan287@gmail.com>
2019-01-15 17:59:01 -0600

    Added a galario@2.rb file to allow multiple python versions with galario to exist.

---
+++ galario@2.rb
@@ -0,0 +1,21 @@
+class GalarioAT2 < Formula
+  desc "GPU Accelerated Library for Analysing Radio Interferometer Observations"
+  homepage "https://mtazzari.github.io/galario/#gpu-accelerated-library-for-analysing-radio-interferometer-observations"
+  head "https://github.com/mtazzari/galario.git"
+
+  depends_on "cmake" => :build
+  depends_on "fftw" => "with-openmp"
+  depends_on "python@2"
+
+  def install
+     ENV.deparallelize  # if your formula fails when building in parallel
+
+    system "mkdir", "build"
+    Dir.chdir("build")
+    system "cmake", *std_cmake_args,
+                    "-DGALARIO_CHECK_CUDA=0",
+                    "-DPython_ADDITIONAL_VERSIONS=2.7", ".."
+    system "make"
+    system "make", "install"
+  end
+end
