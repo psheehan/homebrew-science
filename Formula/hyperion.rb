b087abe4b3e7a55eeb2a645f064a1978d453a061
Patrick Sheehan <psheehan287@gmail.com>
2020-07-07 22:25:00 -0500

    Updated hyperion.rb to use the most recent git commit.

--- Formula/hyperion.rb
+++ Formula/hyperion.rb
@@ -2,14 +2,17 @@ require 'formula'
 
 class Hyperion < Formula
   homepage 'www.hyperion-rt.org'
-  url 'https://pypi.python.org/packages/4f/6a/9693382f131369526c10d396a2371db1782856bed03ebe8500c89939114e/Hyperion-0.9.9.tar.gz'
-  sha256 'e763a99370b1f961fa2c99b8f5c69cdd618787079af6c6cd63de6a953137e501'
+  head 'https://github.com/hyperion-rt/hyperion.git'
 
   depends_on 'hdf5@1.8'
   depends_on 'mpich'
 
   def install
     ENV.deparallelize
+    ENV["HYPERION_HDF5_VERSION"] = "18"
+
+    inreplace "Makefile.in", "FCMPI = @mpi_fc@ @extra_mpi@", 
+        "FCMPI = @mpi_fc@ @extra_mpi@ -fallow-argument-mismatch"
 
     system "./configure", "--disable-debug",
                           "--prefix=#{prefix}"
