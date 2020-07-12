26155e25be3b40d0d36ccdeaf603e5e8934b65a0
Patrick Sheehan <psheehan287@gmail.com>
2020-07-12 16:29:24 -0500

    Updated galario to work with python@3.8

--- Formula/galario.rb
+++ Formula/galario.rb
@@ -5,7 +5,7 @@ class Galario < Formula
 
   depends_on "cmake" => :build
   depends_on "fftw" => "with-openmp"
-  depends_on "python3"
+  depends_on "python@3.8"
 
   def install
      ENV.deparallelize  # if your formula fails when building in parallel
@@ -14,7 +14,7 @@ class Galario < Formula
     Dir.chdir("build")
     system "cmake", *std_cmake_args,
                     "-DGALARIO_CHECK_CUDA=0",
-                    "-DPython_ADDITIONAL_VERSIONS=3.7", ".."
+                    "-DPython_ADDITIONAL_VERSIONS=3.8", ".."
     system "make"
     system "make", "install"
   end
