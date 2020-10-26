ba3ffa0acb1846824cfe30cdc49cf7606ec1bb1a
Patrick Sheehan <psheehan@gmail.com>
2020-10-25 23:09:25 -0500

    Updated to CASA 6.1.0

--- Formula/casa.rb
+++ Formula/casa.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Casa < Formula
   homepage 'http://casa.nrao.edu/'
-  url 'https://casa.nrao.edu/download/distro/linux/release/el7/casa-release-5.1.2-4.el7.tar.gz', :using => :curl
-  sha256 '12fab0c13b217449d21fa9d1494ee8be8a9181ad8c729420e222690835a642dc'
-  version '5.1.2'
+  url 'https://casa.nrao.edu/download/distro/casa/release/rhel/casa-6.1.0-118.tar.xz', :using => :curl
+  sha256 '08dfeeefed56b057f0c62f162848c1e352feb5519b8f3fd4be49944a3bffe13c'
+  version '6.1.0'
   keg_only 'Because it comes with a lot of preinstalled libraries I dont want to link'
 
   option "with-system-xml", "Use the system version of xml rather than the one packaged with CASA."
